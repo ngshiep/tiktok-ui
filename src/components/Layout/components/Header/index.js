ed07110d03bff945ddfa3e4cced75cfa1931d04e
hieppnguyen <n.s.hiep@ttdesignco.com>
2022-06-30 17:12:20 +0700

    create default layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='tiktok'></img>
+                </div>
+
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts or videos' spellCheck = {false}></input>
+                    <button className={cx('clear')}>
+                        {/* clear */}
+                    </button>
+
+                    {/* icon loading */}
+
+                    <button className={cx('search-btn')}>
+
+                    </button>
+                </div>
+            </div>
         </header>
     );
 }
